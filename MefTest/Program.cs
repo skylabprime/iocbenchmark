f06faf23a623c0f9795239fa22cc61754316aefc
Timothée Lecomte <lecomte.timothee@gmail.com>
2015-12-06 19:18:49 +0100

    Add benchmarks for MEF and MEF2.
    MEF2 looks like the big winner here!

---
+++ MefTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using IoCBenchmark;
+
+namespace Mef2Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new TestRunner().Run("Mef", () => new MefAbstraction());
+        }
+    }
+}
